45cdf6f86a37c164b54334afb42cb2c8ae853d3c
Garrett Heath <heathsmail0@gmail.com>
2025-08-31 15:51:43 -0600

    Initial Commit

---
+++ Nubu/NubuApp.swift
@@ -0,0 +1,17 @@
+//
+//  NubuApp.swift
+//  Nubu
+//
+//  Created by Heath, Garrett on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct NubuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NubuTests/NubuTests.swift
@@ -0,0 +1,16 @@
+//
+//  NubuTests.swift
+//  NubuTests
+//
+//  Created by Heath, Garrett on 8/31/25.
+//
+
+import Testing
+
+struct NubuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
