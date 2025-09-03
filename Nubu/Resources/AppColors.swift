a2afd8502096ce20ee3745f432121f095d3b40c6
Garrett Heath <heathsmail0@gmail.com>
2025-09-03 07:53:16 -0600

    added app colors

--- Nubu/Resources/AppColors.swift
+++ Nubu/Resources/AppColors.swift
@@ -5,4 +5,11 @@
 //  Created by Heath, Garrett on 9/3/25.
 //
 
-import Foundation
+import SwiftUI
+
+extension Color {
+    static let teal   = Color(red: 0x66/255, green: 0xAF/255, blue: 0xB5/255) // #66AFB5
+    static let pink   = Color(red: 0xF3/255, green: 0xA6/255, blue: 0xB7/255) // #F3A6B7
+    static let peach  = Color(red: 0xFB/255, green: 0xD2/255, blue: 0xB9/255) // #FBD2B9
+    static let purple = Color(red: 0xAB/255, green: 0x87/255, blue: 0xF6/255) // #AB87F6
+}
