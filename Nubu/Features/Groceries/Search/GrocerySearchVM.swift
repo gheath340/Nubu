5a9778b0601ca95231176c93055afb3727a5c5df
Garrett Heath <heathsmail0@gmail.com>
2025-09-10 06:04:41 -0600

    added load items function for grocery items

--- Nubu/Features/Groceries/Search/GrocerySearchVM.swift
+++ Nubu/Features/Groceries/Search/GrocerySearchVM.swift
@@ -9,9 +9,34 @@ import Foundation
 //What do i want the search screen to look like??
     //Item name     Store   Price per unit
     //Click on item to get more info and to choose to add and how many units to add
-struct GroceryItem: Codable, Identifiable, Equatable {
+struct GroceryItem: Codable, Identifiable, Equatable, Hashable {
     let id: UUID
     let name: String
     let store: String
+    let size_value: Double
+    let size_unit: String
+    let price_cents: Double
+}
+
+@MainActor
+final class GrocerySearchVM: ObservableObject {
+    @Published var groceryItems: [GroceryItem] = []
+    @Published var log: String = ""
+    private var tableName: String = "catalog_items"
     
+    func loadItems() async {
+        do {
+            let rows: [GroceryItem] = try await supabase
+                .from(tableName)
+                .select("*")
+                .execute()
+                .value
+            
+            groceryItems = rows
+            log = "Loaded \(rows.count) rows"
+        } catch {
+            log = "Select error: \(error.localizedDescription)"
+            print(error)
+        }
+    }
 }
